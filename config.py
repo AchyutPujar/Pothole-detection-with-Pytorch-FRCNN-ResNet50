b5ba047cabca4494fe13a9395e1d77eccd6fe3cf
Achyut Pujar <46969186+AchyutPujar@users.noreply.github.com>
2021-06-28 21:00:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,3 @@
-# │   config.py
-# │   dataset.py
-# │   engine.py
-# │   model.py
-# │   test.py
-# │   train.py
-# ├───checkpoints
-# ├───input
-# │   │   PotholeDataset.pdf
-# │   │   train_df.csv
-# │   └───Dataset 1 (Simplex)
-# │       └───Dataset 1 (Simplex)
-# │           ├───Test data
-# │           └───Train data
-# │               ├───Negative data
-# │               └───Positive data
-# ├───test_predictions
-
 ROOT_PATH = 'input/Dataset 1 (Simplex)/Dataset 1 (Simplex)'
 TEST_PATH = 'input/Dataset 1 (Simplex)/Dataset 1 (Simplex)/Test data'
 PREDICTION_THRES = 0.8
